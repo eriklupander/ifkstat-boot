4485895796440e8a0eb4c2d41d56932614d354d7
Erik Lupander <erik.lupander@squeed.com>
2015-09-06 13:30:11 +0200

    etc/init.d service stuff

--- src/test/java/se/ifkgoteborg/stat/util/PasswordUtilTest.java
+++ src/test/java/se/ifkgoteborg/stat/util/PasswordUtilTest.java
@@ -1,14 +1,15 @@
 package se.ifkgoteborg.stat.util;
 
 import junit.framework.Assert;
+import org.testng.annotations.Test;
 
-import org.junit.Test;
+import static org.testng.Assert.assertEquals;
 
 public class PasswordUtilTest {
 
     @Test
     public void testHashPassword() {
         String passwordHash = PasswordUtil.getPasswordHash("5ecret");
-        Assert.assertEquals("CY9rzUYh03PK3k6DJie09g==", passwordHash);
+        assertEquals("Zwngj0HmTxVUeNuY3Pldgw==", passwordHash);
     }
 }
